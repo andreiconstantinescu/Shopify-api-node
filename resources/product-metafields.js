de8e894958de1c404d90b9d558ff985143cbb6b0
Andrei Constantinescu <andrei@kano.me>
2016-07-06 11:34:47 +0100

    Add product metafield entry.

---
+++ resources/product-metafields.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const assign = require('lodash/assign');
+
+const baseChild = require('../mixins/base-child');
+
+/**
+ * Creates a ProductMetafields instance.
+ *
+ * @param {Shopify} shopify Reference to the Shopify instance
+ * @constructor
+ * @public
+ */
+function ProductMetafields(shopify) {
+  this.shopify = shopify;
+
+  this.parentName = 'products';
+  this.name = 'metafields';
+  this.key = 'product';
+}
+
+assign(ProductMetafields.prototype, baseChild);
+
+module.exports = ProductMetafields;
